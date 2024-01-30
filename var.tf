c7c5fc8f02c82d2b9290a7b6fb676a6eccd2f386
VINAY650-hash <79817305+VINAY650-hash@users.noreply.github.com>
2024-01-30 13:18:19 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,17 +1,1 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
 
-variable "instance_type" {
-   type        = string
-   description = "Instance type"
-   default     = "t2.medium"
-}
-
-variable "name_tag" {
-   type        = string
-   description = "Name of the EC2 instance"
-   default     = "My EC2 Instance"
-}
