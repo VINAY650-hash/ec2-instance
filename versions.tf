77ab3c0ed8288819a6f91df6d04b30eeacb0dd94
VINAY650-hash <79817305+VINAY650-hash@users.noreply.github.com>
2024-01-30 12:50:58 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,12 +1,11 @@
 terraform {
-required_version = ">= 1.6"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-} 
+}
 # Provider Block
 provider "aws" {
   region = "us-east-1"
